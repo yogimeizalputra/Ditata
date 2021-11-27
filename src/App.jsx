63d7821f9bc7745c5c9ea29cc83b4e5ca2084d51
YMP-BLACRUSH <67401418+YMP-BLACRUSH@users.noreply.github.com>
2021-11-27 17:30:24 +0700

    Introduction Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import styled from 'styled-components';
+import Intro from './components/Intro';
 import Navbar from './components/Navbar';
 
 const Container = styled.div`
   height: 100vh;
-
+  overflow: hidden;
 `
 
 function App() {
   return (
     <Container>
       <Navbar />
+      <Intro />
     </Container>
   );
 }
