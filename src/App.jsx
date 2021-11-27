47a93eaef4b4a76f25be41f5b74f248442bbd0c7
YMP-BLACRUSH <67401418+YMP-BLACRUSH@users.noreply.github.com>
2021-11-27 16:37:16 +0700

    Navbar Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import styled from 'styled-components';
+import Navbar from './components/Navbar';
+
+const Container = styled.div`
+  height: 100vh;
+
+`
 
 function App() {
   return (
-    <div className="App">
-      Hello
-    </div>
+    <Container>
+      <Navbar />
+    </Container>
   );
 }
 
